d8b4f8793429d9ccebd4c45c9f77e0a178845200
saikrishna-dev <saikrishnacr2012@gmail.com>
2022-11-04 11:25:59 +0000

    forgotten commit

--- features/API/apitest.js
+++ features/API/apitest.js
@@ -1,16 +1,26 @@
 const { spec } = require('pactum');
 
-it('should save a new user', async () => {
+it('should check api health', async () => {
   await spec()
-    .get('http://65.2.171.175:8080/wd-webapp/login')
+    .get('http://3.110.159.152:8080/wd-webapp/health')
     //.post('https://the-internet.herokuapp.com/login')
-    .withForm({
-      username: 'admin',
-      password: '12345'
-    })
-    .expectBodyContains('Success')
-    .expectStatus(201)
+    .expectBodyContains('UP')
+    .expectStatus(200)
 });
+
+it('should verify credentials', async () => {
+  await spec()
+    .post('http://3.110.159.152:8080/wd-webapp/login')
+    //.post('https://the-internet.herokuapp.com/login')
+    .withJson({
+      "username":"admin",
+      "password":"12345"
+  })
+    .expectBodyContains('Success')
+    .expectStatus(200)
+});
+
+
 
 // const { spec } = require('pactum');
 
